a1df06a8a07bcc8c4243a4e3bc6b097c946af978
MADHU <DELL@DESKTOP-5CLC4DE.(none)>
2021-03-02 09:43:13 +0530

    signup page vaidations added

--- src/app/user-actions/signup/signup.component.ts
+++ src/app/user-actions/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
+import { Observable } from 'rxjs';
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -18,18 +19,89 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SignupComponent implements OnInit {
-  
-  emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
 
-  matcher = new MyErrorStateMatcher();
+  formGroup: any | undefined = FormGroup;
+  titleAlert: string | undefined = "This field is required";
+  showPassword = false;
+  showConfirmPassword = false;
+  post: any;
 
-  constructor() { 
-  }
+  // emailFormControl = new FormControl('', [
+  //   Validators.required,
+  //   Validators.email,
+  // ]);
+
+  //matcher = new MyErrorStateMatcher();
+
+  constructor(
+    private _fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.createForm();
+    // this.setChangeValidate();
+    //this.checkConfirmPasswords(this.formGroup);
+  }
+
+  createForm() {
+    let emailregex: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    let nameregex: RegExp = /^([A-Za-z0-9]{6,})*$/
+    this.formGroup = this._fb.group({
+      email: [null, [Validators.required, Validators.pattern(emailregex)], this.checkInUseEmail],
+      name: [null, [Validators.required, Validators.pattern(nameregex)]],
+      password: [null, [Validators.required, this.checkPassword]],
+      confirmPassword: [null, [Validators.required]]
+    }, {
+      validators: this.checkConfirmPasswords.bind(this)
+    });
+  }
+
+
+  checkPassword(control: { value: any; }) {
+    let enteredPassword = control.value
+    let passwordCheck = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
+    return (!passwordCheck.test(enteredPassword) && enteredPassword) ? { 'requirements': true } : null;
+  }
+
+  checkConfirmPasswords(group: any) { // here we have the 'passwords' group
+    const password = group.get('password').value;
+    const confirmPassword = group.get('confirmPassword').value;
+    return password === confirmPassword ? null : { notSame: true }
+  }
+
+  checkInUseEmail(control: { value: string; }) {
+    // mimic http database access
+    let db = ['tony@gmail.com'];
+    return new Observable(observer => {
+      setTimeout(() => {
+        let result = (db.indexOf(control.value) !== -1) ? { 'alreadyInUse': true } : null;
+        observer.next(result);
+        observer.complete();
+      }, 4000)
+    })
+  }
+
+  getErrorEmail() {
+    return this.formGroup.get('email').hasError('required') ? 'Email Address is required' :
+      this.formGroup.get('email').hasError('pattern') ? 'Seems like you’ve entered an invalid email. Please try again.' :
+        this.formGroup.get('email').hasError('alreadyInUse') ? 'This emailaddress is already in use' : '';
+  }
+  getErrorName() {
+    return this.formGroup.get('name').hasError('required') ? 'Incorrect username (e.g. jamesbond007)' :
+      this.formGroup.get('name').hasError('pattern') ? 'Minimum 6 characters using letters and numbers. Avoid using spaces or special characters.' : '';
+  }
+
+  getErrorPassword() {
+    return this.formGroup.get('password').hasError('required') ? 'Field is required (at least eight characters, one uppercase letter and one number)' :
+      this.formGroup.get('password').hasError('requirements') ? 'Password must be a combination of lower case and upper case alphabets, must include at least one number and one special character, and must be at least eight characters long' : '';
+  }
+  getErrorConfirmPassword() {
+    return this.formGroup.get('confirmPassword').hasError('required') ? 'Field is required (at least eight characters, one uppercase letter and one number)' :
+      this.formGroup.get('confirmPassword').hasError('notSame') ? 'Looks like the passwords are not matching. Please try again.' : '';
+  }
+
+  onSubmit(post: any) {
+    this.post = post;
   }
 
 }
